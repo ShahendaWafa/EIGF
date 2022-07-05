ee7889b3d548e2cbcea3a3304e962ab68a0c16d1
HassanAdham <36557564+HassanAdham@users.noreply.github.com>
2022-07-06 01:31:53 +0200

    arabic

---
+++ Assets/Scripts/ArabicTextScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ArabicTextScript : MonoBehaviour
+{
+    [TextArea]
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
